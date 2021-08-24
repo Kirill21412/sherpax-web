2483951adf9a31bdaf97cabe55e748decc62610d
xl2412 <1293358059@qq.com>
2021-08-24 17:33:11 +0800

    add: svg && icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
+import HomePage from "./pages/HomePage/index";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { Suspense } from "react";
 
 const App: React.FC = () =>
 {
   const { i18n } = useTranslation();
   const language = i18n.language;
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
 
-    </Suspense>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+function Home(): React.ReactElement
+{
+    return (
+        <>
+            <p>1212</p>
+        </>
+    );
+}
+
+export default Home;
