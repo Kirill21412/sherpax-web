f23fdc02ea7c8d6d274145a873059a5dabac40f3
xl2412 <1293358059@qq.com>
2021-09-01 01:39:02 +0800

    fix:hover

--- src/components/MenuButton/styles.tsx
+++ src/components/MenuButton/styles.tsx
@@ -39,10 +39,10 @@ body {
 
 .bm-burger-button {
   position: fixed;
-  width: 7vw;
-  height: 7vh;
+  width: 6vw;
+  height: 6vh;
   right: 5vw;
-  margin-top: -6vh;
+  margin-top: -4.6vh;
 }
 
 /* Color/shape of burger icon bars */
@@ -86,7 +86,7 @@ body {
 /* Styling of overlay */
 .bm-overlay {
   background: rgba(0, 0, 0, 0.3);
-  margin-top: -7vh !important;
+  margin-top: -5.7vh !important;
   margin-left: -6vw !important;
 }
 
