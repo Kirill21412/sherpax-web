a6d822df2b8a7f3edb70be43e94781a8701b2e1a
xl <1293358059@qq.com>
2022-03-24 10:10:28 +0800

    pref: add model

---
+++ src/components/ModelPage/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Props
+{
+  leftItem:React.ReactElement,
+  rightItem:React.ReactElement
+}
+
+function KsxItem({ leftItem, rightItem}: Props): React.ReactElement<Props>
+{
+
+  return (
+    <div></div>
+  );
+}
+
+export default KsxItem;
